bfcd6c589317978511ed2604396f2372400e14d0
Matan Nassau <matan.nassau@gmail.com>
2012-04-21 17:27:26 -0400

    tests

--- test/main.cc
+++ test/main.cc
@@ -1,2 +1,33 @@
 #define CATCH_CONFIG_MAIN
 #include <catch/catch.hpp>
+#include <shell_word_split/shell_word_split.hh>
+
+TEST_CASE("input/one_word", "")
+{
+    auto const words = sws::shell_word_split("word");
+    REQUIRE(words.size() == 1);
+    REQUIRE(words[0] == "word");
+}
+
+TEST_CASE("input/two_words", "")
+{
+    auto const words = sws::shell_word_split("two words");
+    REQUIRE(words.size() == 2);
+    REQUIRE(words[0] == "two");
+    REQUIRE(words[1] == "words");
+}
+
+TEST_CASE("input/escaped_space_character", "")
+{
+    auto const words = sws::shell_word_split("\\ ");
+    REQUIRE(words.size() == 1);
+    REQUIRE(words[0] == "\\ ");
+}
+
+TEST_CASE("input/whitespace_mix", "")
+{
+    auto const words = sws::shell_word_split("two 	 words");
+    REQUIRE(words.size() == 2);
+    REQUIRE(words[0] == "two");
+    REQUIRE(words[1] == "words");
+}
