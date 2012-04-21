e75a70dd4537e0b8dd444cbbaca8d6fe2697b2dc
Matan Nassau <matan.nassau@gmail.com>
2012-04-21 15:08:37 -0400

    mv helpers to sws::detail

---
+++ detail/detail.hh
@@ -0,0 +1,32 @@
+#ifndef DETAIL_HH_
+#define DETAIL_HH_
+
+namespace sws { namespace detail {
+inline char const * parse_double_quoted_string(char const * p)
+{
+    while( *p && *++p != '"' )
+        if( *p == '\\' && *(p + 1) == '"' ) ++ ++p;
+    return *p ? p : ++p;
+}
+
+inline char const * parse_single_quoted_string(char const * p)
+{
+    while( *p && *++p != '\'' );
+    return *p ? p : ++p;
+}
+
+inline char const * parse_whitespace(char const * p)
+{
+    while( std::isspace(*p) )
+        ++p;
+    return p;
+}
+
+inline char const * parse_escape(char const * p)
+{
+    return ++ ++p;
+}
+
+} }  // namespace sws::detail
+
+#endif  // DETAIL_HH_
--- shell_word_split.hh
+++ shell_word_split.hh
@@ -4,47 +4,23 @@
 #include <string>
 #include <vector>
 #include <cctype>
+#include "detail/detail.hh"
 
 namespace sws {
-inline char const * parse_double_quoted_string(char const * p)
-{
-    while( *p && *++p != '"' )
-        if( *p == '\\' && *(p + 1) == '"' ) ++ ++p;
-    return *p ? p : ++p;
-}
-
-inline char const * parse_single_quoted_string(char const * p)
-{
-    while( *p && *++p != '\'' );
-    return *p ? p : ++p;
-}
-
-inline char const * parse_whitespace(char const * p)
-{
-    while( std::isspace(*p) )
-        ++p;
-    return p;
-}
-
-inline char const * parse_escape(char const * p)
-{
-    return ++ ++p;
-}
-
 inline std::vector<std::string> shell_word_split(char const * p)
 {
     std::vector<std::string> words;
     char const * t = p;
     while( *p ) {
         if( *p == '"' ) {
-            p = parse_double_quoted_string(p);
+            p = detail::parse_double_quoted_string(p);
         } else if( *p == '\'' ) {
-            p = parse_single_quoted_string(p);
+            p = detail::parse_single_quoted_string(p);
         } else if( *p == '\\' && *(p + 1) ) {
-            p = parse_escape(p);
+            p = detail::parse_escape(p);
         } else if( std::isspace(*p) ) {
             words.push_back(std::string(t, p));
-            t = p = parse_whitespace(p);
+            t = p = detail::parse_whitespace(p);
         } else {
             ++p;
         }
