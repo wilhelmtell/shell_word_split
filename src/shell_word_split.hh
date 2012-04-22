f10d84f4a6ffad5b0a4d816b79a170ad5b8fe4d9
Matan Nassau <matan.nassau@gmail.com>
2012-04-22 13:32:10 -0400

    maintain an in-word state

--- src/shell_word_split.hh
+++ src/shell_word_split.hh
@@ -11,29 +11,36 @@ inline std::vector<std::string> shell_word_split(char const * p)
 {
     std::vector<std::string> words;
     std::string word;
+    bool in_word = false;
+    p = detail::skip_whitespace(p);
     while( *p ) {
         if( *p == '"' ) {
+            in_word = true;
             char const * t = p;
             p = detail::parse_double_quoted_string(p);
             word.append(t + 1, p - 1);
         } else if( *p == '\'' ) {
+            in_word = true;
             char const * t = p;
             p = detail::parse_single_quoted_string(p);
             word.append(t + 1, p - 1);
         } else if( *p == '\\' && *(p + 1) ) {
+            in_word = true;
             char const * t = p;
             p = detail::parse_escape(p);
             word.append(t + 1, p);
         } else if( std::isspace(*p) ) {
-            if( ! word.empty() )
+            if( in_word )
                 words.push_back(word);
+            in_word = false;
             word.clear();
             p = detail::skip_whitespace(p);
         } else {
+            in_word = true;
             word.push_back(*p++);
         }
     }
-    if( ! word.empty() )
+    if( in_word )
         words.push_back(word);
     return words;
 }
