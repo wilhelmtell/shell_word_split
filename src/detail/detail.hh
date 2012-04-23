2d1ec07ab936e32dc4b382e50055cb4ac0fcc9d1
Matan Nassau <matan.nassau@gmail.com>
2012-04-22 23:44:37 -0400

    rewriter parser through fsm

--- src/detail/detail.hh
+++ src/detail/detail.hh
@@ -1,18 +1,30 @@
 #ifndef DETAIL_HH_
 #define DETAIL_HH_
 
+#include <string>
+
 namespace sws { namespace detail {
-inline char const * parse_double_quoted_string(char const * p)
+inline char const * parse_escaping_double_quotes(char const * p, std::string& word)
 {
-    while( *p && *++p != '"' )
-        if( *p == '\\' && *(p + 1) == '"' ) ++p;
-    return *p ? ++p : p;
+    word.push_back(*++p);
+    return p;
 }
 
-inline char const * parse_single_quoted_string(char const * p)
+inline char const * parse_double_quoted_string(char const * p, std::string& word)
 {
-    while( *p && *++p != '\'' );
-    return *p ? ++p : p;
+    while( *p && *++p != '"' )
+        if( *p == '\\' )
+            p = parse_escaping_double_quotes(p, word);
+        else
+            word.push_back(*p);
+    return p;
+}
+
+inline char const * parse_single_quoted_string(char const * p, std::string& word)
+{
+    while( *p && *++p != '\'' )
+        word.push_back(*p);
+    return p;
 }
 
 inline char const * skip_whitespace(char const * p)
@@ -22,9 +34,10 @@ inline char const * skip_whitespace(char const * p)
     return p;
 }
 
-inline char const * parse_escape(char const * p)
+inline char const * parse_bare_escape(char const * p, std::string& word)
 {
-    return ++ ++p;
+    word.push_back(*++p);
+    return p;
 }
 
 } }  // namespace sws::detail
